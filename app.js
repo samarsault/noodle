6d8bad46e4ee45ed7ab13027466a291ed3df94a3
Samarjeet <thelehhman@gmail.com>
2019-07-14 23:36:08 +0530

    404 & No Courses

--- app.js
+++ app.js
@@ -82,22 +82,23 @@ app.use('/api', apiRouter);
 // Admin App
 app.use('/dashboard', express.static(path.join(__dirname, 'client', 'dashboard', 'dist' )));
 
+if (process.env.NODE_ENV !== 'production') {
+  // error handler
+  app.use(function(err, req, res, next) {
+  	// set locals, only providing error in development
+  	res.locals.message = err.message;
+  	res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  	// render the error page
+  	res.status(err.status || 500);
+  	res.json( {
+  		error: err.message
+  	});
+  });
+}
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-	next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-	// set locals, only providing error in development
-	res.locals.message = err.message;
-	res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-	// render the error page
-	res.status(err.status || 500);
-	res.json( {
-		error: err.message
-	});
+app.use(function (req, res, next) {
+  res.render('404');
 });
 
 module.exports = app;
