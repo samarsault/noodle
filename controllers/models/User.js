11da06fc6745532785feab2e74c5b0b9587469d7
Samarjeet <thelehhman@gmail.com>
2019-08-08 23:33:24 +0530

    Uniquity

--- controllers/models/User.js
+++ controllers/models/User.js
@@ -22,13 +22,10 @@ const UserSchema = new Schema({
   bits_id: {
     type: String,
 		match: /\d{4}[ABH](A|[0-9])(PS|([ABH]|[0-9])(A|[0-9]))\d{4}G/,
-		sparse: true,
-    unique: true
   },
   phone: {
     type: String,
-		match: /\d{10}/,
-		sparse: true
+		match: /\d{10}/
   },
   role: {
     type: String,
