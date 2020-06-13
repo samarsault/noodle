0bc09ab8172a564eab1d1f91fc9f5a5f3b25df71
Devanshu24 <devshah.685@live.com>
2020-06-14 02:51:12 +0530

    Added the Card Component

---
+++ dashboard/src/components/Card.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="card">
+    <img src="@/../public/logo.png" alt="CTE Course" style="width: 100%;" />
+    <div class="container">
+      <h4>
+        <b>{{ name }}</b>
+      </h4>
+      <p>
+        {{ info }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Card",
+  props: ["name", "info"],
+};
+</script>
+
+<style lang="scss" scoped>
+.card {
+  padding: 0px;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  transition: 0.3s;
+  width: 20%;
+  height: 10%;
+  margin: 20px;
+}
+
+.card:hover {
+  box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
+}
+
+.container {
+  padding: 2px 16px;
+}
+</style>
