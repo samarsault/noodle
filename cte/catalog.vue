8bdeace24abbc836fc80952d95c52b15fcd0b5c3
Samarjeet <samarsault@gmail.com>
2020-07-19 00:52:52 +0530

    Curriculum & course page

--- cte/catalog.vue
+++ cte/catalog.vue
@@ -1,24 +1,22 @@
 <template lang="pug">
-div.container
-  h1 Courses 
-
-  .courses
-    .course-card(v-for="course in courses")
-      .card
-        .card-header
-          img(:src="course.coverImage", alt="Thumbnail")
-          h2 {{course.name}}
-        .card-body
-          p {{course.subtitle}}
-        .card-footer
-          button.secondary(@click='register(course._id)') Register
+section#courses
+  .container
+    h1 Courses
+    .courses
+      CourseCard(v-for="course in courses", :course="course", :key="course._id", style="max-height: 400px")
+        template(slot='action')
+          p(style='margin-top: 0') {{ course.subtitle }}
+          router-link(:to="`/course/${course._id}`")
+            button.secondary View
 </template>
-
 <script>
 import coursesApi from '@/api/courses';
-import courseApi from '@/api/course';
+import CourseCard from '@/components/CourseCard'
 
 export default {
+  components: {
+    CourseCard
+  },
   data() {
     return {
       courses: []
@@ -27,155 +25,24 @@ export default {
   async created() {
     this.courses = await coursesApi.getAll();
   },
-  methods: {
-    async register(course_id) {
-      const registerSuccess = await courseApi(course_id).register();
-      if (registerSuccess)
-        this.$router.push({ path: '/dashboard' })
-      else
-        alert('Error registering.')
-    }
-  }
 }
 </script>
-
 <style lang="scss">
-.catalog {
-	position: relative;
-	button {
-		width: 120px;
-		margin-bottom: 15px;
-  }
-  #heading {
-		color: #fff;
-		margin-top: 0;
-    padding-top: 1.4em;
-    margin-bottom: .5em;
-	}
-	margin-bottom: 50px;
-}
 .courses {
-	display: flex;
-	flex-wrap: wrap;
-	@media screen and (max-width: 720px) {
-		justify-content: center;
-	}
-	.course-card {
-		flex-basis: 33.33%;
-		padding: 10px;
-		@media screen and (max-width: 1220px) and (min-width: 720px){
-			flex-basis: 50%;	
-		}
-		@media screen and (max-width: 720px) {
-			flex-basis: 80%;	
-		}
-		@media screen and (max-width: 540px){
-			flex-basis: 100%;	
-		}
-		.card {
-			width: 100%;
-			margin: 0;
-		}
-	}
-
-  // .card {
-  //   transition: transform .3s ease;
-  //   &:hover {
-  //     transform: scale(1.02);
-  //   }
-  // }
-}
-
-.course-hero {
-	display: flex;
-	@media screen and (max-width: 920px) {
-		flex-direction: column;
-	}
-	img {
-		max-width: 400px;
-	}
-	.controls {
-		padding: 0;
-		button {
-			margin-right: 10px;
-			&:first-child {
-				margin-left: 0;
-			}
-		}
-	}
-	.info {
-		@media screen and (max-width: 920px) {
-			margin: 20px 0;
-		}
-		margin: 0 40px;
-
-		p {
-			margin-top: 0;
-		}
-	}
-}
-
-.card {
-	background-color: #fff;
-	box-shadow: 0 0 2px 0 rgba(0,0,0,0.35);
-	width: 375px;
-	margin: 20px;
-  
-	.card-header {
-		text-align: center;
-		img {
-			width: 100%;
-			border-bottom: 8px solid $green;
-		}
-	}
-	.card-body, .card-header h2 {
-		padding: 0 30px;
-	}
-	.card-footer {
-		padding-bottom: 15px;
-		text-align: center;
-	}
-}
-
-.course-info {
-	margin: 20px 0;
-}
-
-.course-info .container {
-	display: flex;
-
-	@media screen and (max-width: 960px){
-		flex-direction: column;		
-		.card {
-			width: 100%;
-			margin-left: 0;
-		}
-	}
-
-  .card {
-		padding: 15px 30px;
-		@media screen and (max-width: 1200px) and (min-width: 960px){
-			width: 280px;	
-		}
-	}
-
-	#description {
-		width: 100%;
-		margin-left: 0;
-		height: 110%;
-		p {
-			white-space: pre-line;
-		}
-	}
-
-  h3 {
-    margin: .5em 0;
-	}
-	ul {
-		padding-inline-start: 35px;
-		li {
-			padding: 5px 0;
-		}
-	}
+  display: flex;
+  button {
+    width: 90px;
+    margin: 0 auto;
+    display: block;
+  }
+  @media screen and (max-width: 720px) {
+    flex-direction: column;
+    .card {
+      width: calc(100% - 40px);
+    }
+    img {
+      height: 240px !important;
+    }
+  }
 }
 </style>
--- cte/course.vue
+++ cte/course.vue
@@ -1,15 +1,111 @@
-<template lang="pug">
- <h1>{{ item }}</h1> 
+<template>
+  <div class="container" v-if="course">
+    <h1>{{ course.name }}</h1> 
+    <div class="course-view ">
+      <p>{{ course.description }}</p>
+      <img width="480" :src="course.coverImage">
+    </div>
+    <div style="margin-bottom: 30px">
+      <router-link :to="`/dashboard/course/${course._id}`" v-if="courseRegistered">
+        <button v-if="courseRegistered" class="primary">Go To Course</button>
+      </router-link>
+      <button v-else class="primary" @click="register(course._id)">Enroll</button>
+    </div>
+    <div v-if="handout" class="handout">
+      <h2>Curriculum</h2>
+      <div v-for="item in Object.keys(handout)" :key="item" class="handout-item">
+        <p>{{ item }}</p>
+        <ul class="handout-list">
+          <li v-for="page in handout[item]" :key="page._id">{{ page.name }}</li>
+        </ul>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 import axios from 'axios';
+import courseApi from '@/api/course';
+import coursesApi from '@/api/courses';
+import { getters } from '@/utils/store'
 
 export default {
   data() {
     return {
-      item: this.$route.params.course_id
+      course_id: this.$route.params.course_id,
+      course: {},
+      handout: null
+    }
+  },
+  computed: {
+    ...getters,
+    courseRegistered() {
+      if (!this.user)
+        return false;
+      return this.user.courses.includes(this.course._id);
+    }
+  },
+  async created() {
+    this.course = await coursesApi.getCourse(this.course_id)
+    this.handout = await coursesApi.getHandout(this.course_id);
+    if (Object.keys(this.handout).length === 0) 
+      this.handout = null;
+  },
+  methods: {
+    async register(course_id) {
+      try {
+      const registerSuccess = await courseApi(course_id).register();
+      if (registerSuccess) {
+        this.$router.push({
+          path: '/dashboard'
+        })
+      } else {
+        alert('Error registering. Please ensure that you are logged in.')
+      }
+      } catch(e) {
+        alert('Error registering. Please ensure that you are logged in.')
+      }
     }
   }
 }
 </script>
+<style lang="scss" scoped>
+.course-view {
+  display: flex;
+  > img {
+    padding: 20px;
+  }
+  @media screen and (max-width: 720px) {
+    flex-direction: column-reverse;
+    > img {
+      padding: 0;
+    }
+  }
+}
+ul {
+  padding: 0;
+  list-style-type: none;
+}
+.handout {
+  max-width: 640px;
+  p {
+    margin-top: 0;
+    font-weight: bold;
+    padding-bottom: 15px;
+    border-bottom: 1px solid #eee;
+  }
+  &-item {
+    background-color: #fff;
+    border: 1px solid rgba(33,36,44,0.16);
+    border-radius: 4px;
+    padding: 20px;
+    margin: 10px 0;
+  }
+}
+.handout-list {
+  color: rgba($black, 0.7);
+  li {
+    margin: 10px 0;
+  }
+}
+</style>
